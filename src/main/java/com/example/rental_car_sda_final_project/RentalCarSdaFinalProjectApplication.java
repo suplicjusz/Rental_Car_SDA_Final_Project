0fec1f680a2f9602081bcca10cd898f1f3c7193b
adasu <supel.dev@gmail.com>
2022-05-16 20:10:00 +0200

    Initial commit

---
+++ src/main/java/com/example/rental_car_sda_final_project/RentalCarSdaFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rental_car_sda_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarSdaFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCarSdaFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rental_car_sda_final_project/RentalCarSdaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rental_car_sda_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarSdaFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
